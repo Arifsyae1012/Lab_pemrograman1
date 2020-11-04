0e03cc97e9d20637d38e5e72d16a626e599abb7d
Arifsyae1012 <73016066+Arifsyae1012@users.noreply.github.com>
2020-11-04 21:34:40 +0700

    Add files via upload

---
+++ Lab2.py
@@ -0,0 +1,11 @@
+a=input("masukkan Nilai a : ")
+b=input("masukkan Nilai b : ")
+print("variable a = ", a)
+print("variable b = ", b)
+print("Hasil penggabungan {1}&{0}=%d".format(a,b) %(a+b))
+
+# Konversi nilai Variable
+a=int(255)
+b=int(255)
+print("Hasil penjumlahan {1}+{0}=%d".format(a,b) %(a+b))
+print("Hasil pembagian {1}/{0}=%d".format(a,b) %(a/b))
